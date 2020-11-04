02562674277e32d2d56e1ffe1270c4e38d20436a
tharcissie <tharcissieidufashe@gmail.com>
2020-11-04 05:45:14 +0200

    statics

---
+++ static/js/evaluator.js
@@ -0,0 +1,55 @@
+
+
+let input,hello,filter,table,tr,td,i,txtValue;
+
+input = document.getElementById('input-mtef');
+filter = input.value.toUpperCase();
+table = document.getElementById('tableID');
+tr = table.getElementsByTagName("tr");
+hello = document.getElementById('helloo');
+
+
+input.addEventListener('keyup', function(){
+
+///////////// testing purpose
+    hello.style.color = 'red';
+///////////// testing purpose
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+})
+
+
+
+
+
+// function myFunction() {
+//     // Declare variables
+//     var input, filter, table, tr, td, i, txtValue;
+//     input = document.getElementById("myInput");
+//     filter = input.value.toUpperCase();
+//     table = document.getElementById("myTable");
+//     tr = table.getElementsByTagName("tr");
+  
+//     // Loop through all table rows, and hide those who don't match the search query
+//     for (i = 0; i < tr.length; i++) {
+//       td = tr[i].getElementsByTagName("td")[0];
+//       if (td) {
+//         txtValue = td.textContent || td.innerText;
+//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//           tr[i].style.display = "";
+//         } else {
+//           tr[i].style.display = "none";
+//         }
+//       }
+//     }
+//   }
