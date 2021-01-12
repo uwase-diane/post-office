161aaa57adf7ce6e57817bb692cdc63e45244926
uwase-diane <diane.uwase13@gmail.com>
2021-01-12 12:04:30 -0500

    made some changes

--- postapp/utils.py
+++ postapp/utils.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-
+#tracking number 
 def generate_code(digits=8):
     key = "".join(random.choices(string.ascii_uppercase + string.digits, k=digits))
     return key
